59bbe8b01b5c05d570c90df19697753621de3394
Lenovezhou <271130940@qq.com>
2017-08-17 14:23:10 +0800

    Hololens 读使用浏览器更新配置文件，读取本地IP

--- Assets/Scripts/HololensHotUpdate/Test.cs
+++ Assets/Scripts/HololensHotUpdate/Test.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,39 +14,76 @@ using System.IO;
 
 public class Test : MonoBehaviour
 {
-    string str = "Null";
-    // Use this for initialization
+    string str = "";
+    private List<string> localIPs;
+
     void Start()
     {
         ReadData();
-        //GameObject.Find("Canvas/Text").GetComponent<Text>().text = ReadData();
+        ReadIp();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    /// <summary>
+    /// Hololens或PC读取本地IP
+    /// </summary>
+    void ReadIp()
     {
+#if UNITY_EDITOR
+        // Get the ip of the Hololens to see if this device is the Holographic Camera device.
+        localIPs = new List<string>();
+        IPAddress[] ips = Dns.GetHostAddresses(Dns.GetHostName());
+        foreach (IPAddress ip in ips)
+        {
+            if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                localIPs.Add(ip.ToString().Trim());
+            }
+        }
 
+#endif
+
+#if NETFX_CORE
+            IReadOnlyList<HostName> hosts = NetworkInformation.GetHostNames();
+            foreach (HostName aName in hosts)
+            {
+                if (aName.Type == HostNameType.Ipv4)
+                {
+                    localIPs.Add(aName.ToString().Trim());
+                }
+            }
+#endif
+
+        foreach (string ip in localIPs)
+        {
+            Debug.Log("Local IP: " + ip);
+            GameObject.Find("Canvas/Text").GetComponent<Text>().text = "本地IP:" + ip;
+        }
     }
 
-#if !NETFX_CORE   //非UWP下  
+
+
+    /// <summary>
+    ///Hololens读取浏览器上的文件
+    /// </summary>
     private void ReadData()
     {
+#if !NETFX_CORE
         GameObject.Find("Canvas/Text").GetComponent<Text>().text = "Test";
-    }
 #else
-      private async void ReadData()
-        {
-            StorageFolder docLib = ApplicationData.Current.LocalFolder;
-            // var docFile = docLib.OpenStreamForReadAsync("\\Test20170815.txt");
-            // 获取应用程序数据存储文件夹
+        StorageFolder docLib = ApplicationData.Current.LocalFolder;
+        // var docFile = docLib.OpenStreamForReadAsync("\\Test20170815.txt");
+        // 获取应用程序数据存储文件夹
 
-            Stream stream = await docLib.OpenStreamForReadAsync("\\Test20170815.txt");
+        Stream stream = await docLib.OpenStreamForReadAsync("\\Test20170815.txt");
 
-            // 获取指定的文件的文本内容
-            byte[] content = new byte[stream.Length];
-            await stream.ReadAsync(content, 0, (int)stream.Length);
+        // 获取指定的文件的文本内容
+        byte[] content = new byte[stream.Length];
+        await stream.ReadAsync(content, 0, (int)stream.Length);
 
-             GameObject.Find("Canvas/Text").GetComponent<Text>().text  = Encoding.UTF8.GetString(content, 0, content.Length);
-        }
+        GameObject.Find("Canvas/Text").GetComponent<Text>().text  = Encoding.UTF8.GetString(content, 0, content.Length);
+       
+
 #endif
+    }
 }
